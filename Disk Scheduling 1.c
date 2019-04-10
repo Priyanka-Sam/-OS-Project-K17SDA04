f20acaf0d9aef78595a10825f7905c5e7ec47196
Priyanka-Sam <46075615+Priyanka-Sam@users.noreply.github.com>
2019-04-10 19:45:19 +0530

    Add files via upload

---
+++ Disk Scheduling 1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<unistd.h>
+
+void main(){
+			int total_dist=0;
+			int current=143;
+			int previous=125;
+			int queue[] = {86, 1470, 913, 1774, 948, 1509, 1022, 1750, 130};
+			
+			}
