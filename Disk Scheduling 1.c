5d4928f1ef9d539b2e9cd7d7d1957c7b0d456dcb
Priyanka-Sam <46075615+Priyanka-Sam@users.noreply.github.com>
2019-04-15 00:19:03 +0530

    Update Disk Scheduling 1.c

--- Disk Scheduling 1.c
+++ Disk Scheduling 1.c
@@ -13,3 +13,16 @@ void getdata();
 void move();
 	};
 
+void sstf::getdata()
+{
+	
+cout<<"Enter the current position of head ";
+    cin>>current;
+    cout<<"Enter the size of queue : ";
+    cin>>size;
+    cout<<"Enter the elements of Queue : ";
+    for(int i=0;i<size;i++)
+        	cin>>queue[i];
+    //86,1470,913,948,1774,1509,1022,1750,130
+}
+
